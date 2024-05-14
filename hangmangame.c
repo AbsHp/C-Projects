39abfa67b9fb7ff986dab5ab4e03f0532ee92405
AbsHp <asinghal21@iitk.ac.in>
2024-05-14 19:44:31 +0530

    added shape

--- hangmangame.c
+++ hangmangame.c
@@ -48,3 +48,94 @@ int strList(char sortedArr[][SIZE_CHARECTERS], char mainStr[], char hint[], char
     return wordNum;
 }
 
+void shape(int stage){
+
+    const int ZERO = 0, ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6;
+
+    if(stage == ONE){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            printf("|                |\n");
+        }
+        printf("|________________|\n");
+    }
+
+    else if(stage == TWO){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            if(t == ONE || t == TWO){
+                printf("|  **            |\n");
+            }
+            else{
+                printf("|                |\n");
+            }
+        }
+        printf("|________________|\n");
+    }
+
+    else if(stage == THREE){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            if(t == ONE || t == TWO){
+                printf("|  **        **  |\n");
+            }
+            else{
+                printf("|                |\n");
+            }
+        }
+        printf("|________________|\n");
+    }
+
+    else if(stage == FOUR){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            if(t == ZERO){
+                printf("|  --            |\n");
+            }
+            else if(t == ONE || t == TWO){
+                printf("|  **        **  |\n");
+            }
+            else{
+                printf("|                |\n");
+            }
+        }
+        printf("|________________|\n");
+    }
+
+    else if(stage == FIVE){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            if(t == ZERO){
+                printf("|  --        --  |\n");
+            }
+            else if(t == ONE || t == TWO){
+                printf("|  **        **  |\n");
+            }
+            else{
+                printf("|                |\n");
+            }
+        }
+        printf("|________________|\n");
+    }
+
+    else if(stage == SIX){
+        printf(" _________________\n");
+        for(int t = 0; t < FIVE; t++){
+            if(t == ZERO){
+                printf("|  --        --  |\n");
+            }
+            else if(t == ONE || t == TWO){
+                printf("|  **        **  |\n");
+            }
+            else if(t == FOUR){
+                printf("| \\/\\/\\/\\/\\/\\/\\  |\n");
+            }
+            else{
+                printf("|                |\n");
+            }
+        }
+        printf("|________________|\n");
+    }
+
+}
+
