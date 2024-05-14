f2b864587113c0501e08273a5f2235ad480ad94b
AbsHp <asinghal21@iitk.ac.in>
2024-05-14 19:45:09 +0530

    added game

--- hangmangame.c
+++ hangmangame.c
@@ -139,3 +139,124 @@ void shape(int stage){
 
 }
 
+void game(char sortedArr[][SIZE_CHARECTERS], int option, int lenOfOption, char hint[]){
+
+    int stage = 1;
+
+    int hintFlag = 1;
+    int outFlag = 1;
+    int correctFlag = 1;
+
+
+    char answers[SIZE_CHARECTERS];
+
+    int letIndex = 0;
+
+    char choice = 0;
+    char enter;
+
+    const char UNDERLINE = '_', SPACE = ' ', ASTERISK = '*' ;
+
+    char line[lenOfOption];
+
+    for(int i = 0; i < lenOfOption; i++){
+
+        if(sortedArr[option][i] != SPACE){
+            line[i] = UNDERLINE;
+        }
+
+        else{
+            line[i] = SPACE;
+        }
+    }
+
+    const int ZERO = 0, ONE = 1, SIX = 6;
+
+    while(ONE){
+
+        shape(stage);
+
+        if (strcmp(line, sortedArr[option]) == ZERO){
+            printf("The word is %s, good job!", sortedArr[option]);
+            break;
+        }
+
+        for(int i = 0; i < lenOfOption; i++){
+            printf("%c", line[i]);
+        }
+
+        if(hintFlag == ONE){
+            printf("\ndo you want a clue? press -> *");
+        }
+
+        printf("\nThe letters that you already tried:");
+
+        for(int k = 0; k < letIndex;k++){
+            if(k == letIndex - ONE){
+                printf(" %c", answers[k]);
+            }
+            else{
+                printf(" %c,", answers[k]);
+            }
+        }
+
+        printf("\nplease choose a letter:\n");
+
+        scanf("%*[^\n]");
+        scanf("%*[^\n]%*c");
+        scanf("%c %c", &enter, &choice);
+
+        if(choice == ASTERISK && hintFlag == ONE){
+            printf("the clue is: ");
+            for(int i = 0; i < strlen(hint); i++){
+                printf("%c", hint[i]);
+            }
+            printf(".\n");
+            hintFlag = 0;
+        }
+
+        else if(choice != ASTERISK){
+
+            for(int i = 0; i < letIndex; i++){
+
+                if(choice == answers[i]){
+                    outFlag = 0;
+                }
+            }
+
+            if(outFlag == ONE){
+
+                answers[letIndex] = choice;
+                letIndex++;
+
+
+                for(int i = 0; i < lenOfOption; i++){
+                    if(sortedArr[option][i] == choice){
+                        line[i] = choice;
+                        correctFlag = 0;
+                    }
+                }
+
+                if(correctFlag == ONE){
+
+                    stage++;
+
+                    if(stage == SIX){
+                        shape(stage);
+                        printf("The word is %s, GAME OVER!", sortedArr[option]);
+                        break;
+                    }
+                }
+            }
+
+            else{
+                printf("You've already tried that letter.\n");
+            }
+
+        }
+
+        outFlag = 1;
+        correctFlag = 1;
+    }
+}
+
