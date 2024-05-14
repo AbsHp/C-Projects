bb24be7a9a4791553714831994143a1732338ed6
AbsHp <asinghal21@iitk.ac.in>
2024-05-14 19:40:25 +0530

    added sort func

---
+++ hangmangame.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE_NAME 16
+#define SIZE_CHARECTERS 21
+
+void sort(char sortedArr[][SIZE_CHARECTERS], int wordNum){
+
+    char temp[SIZE_CHARECTERS];
+    const int ONE = 1;
+
+    for(int k = 0; k < wordNum - ONE; k++){
+
+        for(int j = k + ONE; j < wordNum; j++){
+
+            if(strcmp(sortedArr[k], sortedArr[j]) > 0){
+
+                strcpy(temp, sortedArr[k]);
+                strcpy(sortedArr[k], sortedArr[j]);
+                strcpy(sortedArr[j], temp);
+            }
+        }
+    }
+}
